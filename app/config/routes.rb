48c55d117d8b4b654ac07be83bd4cc378d30dc47
vasanth16 <vasanthrajvr@gmail.com>
2018-10-20 00:10:39 -0400

    Login page working

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+#<<<<<<< HEAD
   devise_for :users
-=======
+#=======
   get '/', to: "welcome#show"
->>>>>>> 1332681595a73e4197e5fa45e5435ece9a46d0ae
+#>>>>>>> 1332681595a73e4197e5fa45e5435ece9a46d0ae
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
